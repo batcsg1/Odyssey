474cb93812d2b6de232a0c7c9f81d57fb31bf5cd
SamBat2005 <batcsg1@student.op.ac.nz>
2025-03-17 20:22:33 +1300

    feat(js): added code for constellation middleware

---
+++ middleware/validation/constellation.js
@@ -0,0 +1,50 @@
+import Joi from "joi";
+
+const constellationSchema = Joi.object({
+  id: Joi.alternatives().try(Joi.string(), Joi.number()).required().messages({
+    "string.base": "id should be a string",
+    "number.base": "id should be a number",
+    "any.required": "id is required"
+  }),
+  right_ascension: Joi.number().required().messages({
+    "number.base": "right_ascension should be a number",
+    "any.required": "right_ascension is required"
+  }),
+  declination: Joi.number().required().messages({
+    "number.base": "declination should be a number",
+    "any.required": "declination is required"
+  }),
+  shape: Joi.string().min(3).max(100).required().messages({
+    "string.base": "shape should be a string",
+    "string.empty": "shape cannot be empty",
+    "string.min": "shape should have a minimum length of {#limit}",
+    "string.max": "shape should have a maximum length of {#limit}",
+    "any.required": "shape is required"
+  }),
+  area: Joi.number().min(0).required().messages({
+    "number.base": "area should be a number",
+    "number.min": "area should be greater than or equal to {#limit}",
+    "any.required": "area is required"
+  }),
+});
+
+const validateSchema = (schema, isRequired = false) => {
+  return (req, res, next) => {
+    const { error } = isRequired
+      ? schema.required().validate(req.body)
+      : schema.validate(req.body);
+
+    if (error) {
+      return res.status(409).json({
+        msg: error.details[0].message,
+      });
+    }
+
+    next();
+  };
+};
+
+const validatePostConstellation = validateSchema(constellationSchema, true);
+const validatePutConstellation = validateSchema(constellationSchema);
+
+export { validatePostConstellation, validatePutConstellation };
