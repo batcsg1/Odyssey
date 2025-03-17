4a429275c288901013b18a61d446c0db9408e115
SamBat2005 <batcsg1@student.op.ac.nz>
2025-03-17 19:51:46 +1300

    feat(js): updated swagger title and description

---
+++ app.js
@@ -0,0 +1,59 @@
+/**
+ * @file Manages everything related to the API
+ * @author Samuel Batchelor
+ */
+
+import express from "express";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+//Importing routes
+
+import { isContentTypeApplicationJSON } from "./middleware/utils.js";
+
+// Create an Express application
+const app = express();
+
+// Use the PORT environment variable or 3000
+const PORT = process.env.PORT || 3000;
+
+app.use(isContentTypeApplicationJSON);
+
+app.use(express.urlencoded({ extended: false })); 
+app.use(express.json()); 
+
+// This should be declared under - const swaggerOptions = { ... };
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "CelestiDB",
+      version: "1.0.0",
+      description: "API for data about the cosmos",
+      contact: {
+        name: "Samuel Batchelor",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./routes/v1/*.js", "./swagger/*.js"]
+};
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+
+app.use("/api/v1/institutions", institutionRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// Start the server on port 3000
+app.listen(PORT, () => {
+  console.log(
+    `Server is listening on port ${PORT}. Visit http://localhost:${PORT}`,
+  );
+});
+
+
+export default app;
