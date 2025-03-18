f0af428c90faa8e03568f5b52e866d4f7ec620af
SamBat2005 <batcsg1@student.op.ac.nz>
2025-03-19 10:37:32 +1300

    feat(js): added stars routes file

---
+++ routes/v1/stars.js
@@ -0,0 +1,35 @@
+/**
+ * @file Defines the route for getting the stars data
+ * @author Samuel Batchelor
+ */
+
+import createRouter from "./base.js";
+
+import {
+  createStar,
+  getStars,
+  getStar,
+  updateStar,
+  deleteStar,
+} from "../../controllers/v1/star.js";
+
+import {
+  validatePostStar,
+  validatePutStar,
+} from "../../middleware/validation/star.js";
+
+const starController = {
+  get: getStars,
+  getById: getStar,
+  create: createStar,
+  update: updateStar,
+  delete: deleteStar,
+};
+
+const starRouter = createRouter(
+  starController,
+  validatePostStar,
+  validatePutStar,
+);
+
+export default starRouter;
