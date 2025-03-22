da71a8c492ba3666509cfce1ce30136a7c7b33b1
SamBat2005 <batcsg1@student.op.ac.nz>
2025-03-22 22:43:06 +1300

    refactor(js): removed swagger documentation from index.js

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -12,21 +12,6 @@ import { getIndex } from "../../controllers/v1/index.js";
 // Create an Express router
 const router = express.Router();
 
-/**
- * @swagger
- * /:
- *   get:
- *     summary: Retrieve test data
- *     tags:
- *       - Main
- *     description: Returns test data from the index route.
- *     responses:
- *       200:
- *         description: A JSON object containing the test data.
- *       500:
- *         description: Internal server error if the request fails.
- */
-
 // Create a GET route
 router.get("/", getIndex);
 
