0b54e4a307909419576d796d98a8198d1c48aa29
SamBat2005 <batcsg1@student.op.ac.nz>
2025-03-24 11:41:18 +1300

    feat(js): added controller file for asteroid model

---
+++ controllers/v1/asteroid.js
@@ -0,0 +1,95 @@
+/**
+ * @file Manages business logic for asteroids
+ * @author Samuel Batchelor
+ */
+
+import Repository from "../../repositories/generic.js";
+
+const asteroidRepository = new Repository("Asteroid");
+
+const selectObject = {
+  id: true,
+  name: true,
+  age: true,
+  mass: true,
+  diameter: true,
+  density: true,
+  type: true,
+  year: true,
+  perigee: true,
+  apogee: true,
+  location: true,
+  starId: true,
+};
+
+const createAsteroid = async (req, res) => {
+  try {
+    const newAsteroid = await asteroidRepository.create(req.body);
+    return res.status(201).json({
+      message: "Asteroid successfully created",
+      data: newAsteroid,
+    });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+
+const getAsteroids = async (req, res) => {
+  try {
+    const asteroids = await asteroidRepository.findAll(selectObject);
+    if (!asteroids.length) {
+      return res.status(404).json({ message: "No asteroids found" });
+    }
+    return res.status(200).json({ data: asteroids });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+
+const getAsteroid = async (req, res) => {
+  try {
+    const asteroid = await asteroidRepository.findById(req.params.id, selectObject);
+    if (!asteroid) {
+      return res.status(404).json({ message: `No asteroid with id: ${req.params.id} found` });
+    }
+    return res.status(200).json({ data: asteroid });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+
+const updateAsteroid = async (req, res) => {
+  try {
+    const asteroid = await asteroidRepository.update(req.params.id, req.body);
+    if (!asteroid) {
+      return res.status(404).json({ message: `No asteroid with id: ${req.params.id} found` });
+    }
+    return res.status(200).json({
+      message: `Asteroid with id: ${req.params.id} successfully updated`,
+      data: asteroid,
+    });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+
+const deleteAsteroid = async (req, res) => {
+  try {
+    const asteroid = await asteroidRepository.findById(req.params.id);
+    if (!asteroid) {
+      return res.status(404).json({ message: `No asteroid with id: ${req.params.id} found` });
+    }
+    await asteroidRepository.delete(req.params.id);
+    return res.status(200).json({ message: `Asteroid with id: ${req.params.id} successfully deleted` });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+
+export {
+  createAsteroid,
+  getAsteroids,
+  getAsteroid,
+  updateAsteroid,
+  deleteAsteroid,
+};
