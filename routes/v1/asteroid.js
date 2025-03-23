081c39732a0e253bbd4d126925d58862e034a68d
SamBat2005 <batcsg1@student.op.ac.nz>
2025-03-24 11:59:31 +1300

    feat(js): added asteroids route file

---
+++ routes/v1/asteroid.js
@@ -0,0 +1,35 @@
+/**
+ * @file Defines the route for getting the asteroid data
+ * @author Samuel Batchelor
+ */
+
+import createRouter from "./base.js";
+
+import {
+  createAsteroid,
+  getAsteroids,
+  getAsteroid,
+  updateAsteroid,
+  deleteAsteroid,
+} from "../../controllers/v1/asteroid.js";
+
+import {
+  validatePostAsteroid,
+  validatePutAsteroid,
+} from "../../middleware/validation/asteroid.js";
+
+const asteroidController = {
+  get: getAsteroids,
+  getById: getAsteroid,
+  create: createAsteroid,
+  update: updateAsteroid,
+  delete: deleteAsteroid,
+};
+
+const asteroidRouter = createRouter(
+  asteroidController,
+  validatePostAsteroid,
+  validatePutAsteroid,
+);
+
+export default asteroidRouter;
