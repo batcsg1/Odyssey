58a0d8aa000faf73bd19dcc45f1b36fbaf9d33f4
SamBat2005 <batcsg1@student.op.ac.nz>
2025-03-27 20:17:22 +1300

    feat(js): added star id validation in asteroid controllers file

--- controllers/v1/asteroid.js
+++ controllers/v1/asteroid.js
@@ -26,6 +26,14 @@ const selectObject = {
 
 const createAsteroid = async (req, res) => {
   try {
+    const starId = req.body.starId;
+
+    // Check if star exists
+    const star = await new Repository("Star").findById(starId);
+    if (!star) {
+      return res.status(404).json({ message: `Star with id ${starId} not found` });
+    }
+    
     const newAsteroid = await asteroidRepository.create(req.body);
     return res.status(201).json({
       message: "Asteroid successfully created",
