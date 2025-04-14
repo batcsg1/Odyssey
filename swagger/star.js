e0a4365bd27a4ba21700e88913b335ccb6bfa7bd
SamBat2005 <batcsg1@student.op.ac.nz>
2025-04-14 15:44:41 +1200

    docs: added galaxy id to star swagger

--- swagger/star.js
+++ swagger/star.js
@@ -50,6 +50,10 @@
  *           type: string
  *           format: uuid
  *           example: "123e4567-e89b-12d3-a456-426614174000"
+ *         galaxyId:
+ *           type: string
+ *           format: uuid
+ *           example: "123e4567-e89b-12d3-a456-426614174000"
  */
 
 /**
