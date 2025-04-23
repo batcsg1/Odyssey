fd89bc65953d822b76d9f8df251922ef3ae99d18
SamBat2005 <batcsg1@student.op.ac.nz>
2025-04-23 21:08:11 +1200

    styles: added code omment next to obj in seeding normal users file

--- prisma/seeding/csv-seeding/seed-normal-users.js
+++ prisma/seeding/csv-seeding/seed-normal-users.js
@@ -2,15 +2,7 @@ import fs from 'node:fs';
 import { csvToObj } from 'csv-to-js-parser'
 
 const file = fs.readFileSync('prisma/seeding/seeding-files/normal.csv', 'utf8');
-console.log(file)
 
-let obj = JSON.stringify(csvToObj(file, ';'), null, 2);
+let obj = JSON.stringify(csvToObj(file, ';'), null, 2); // Convert the array of objects into JSON format
 console.log(obj)
-
-
-
-
-
-
-
 
