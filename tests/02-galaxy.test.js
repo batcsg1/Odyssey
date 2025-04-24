69bcec9ebfb2b84effe7144779e863a7dc5a15df
SamBat2005 <batcsg1@student.op.ac.nz>
2025-04-24 20:50:13 +1200

    feat: added galaxy test file

---
+++ tests/02-galaxy.test.js
@@ -0,0 +1,155 @@
+import * as chaiModule from "chai";
+import chaiHttp from "chai-http";
+import { describe, it } from "mocha";
+
+import app from "../app.js";
+
+const chai = chaiModule.use(chaiHttp);
+
+import { GalaxyType } from "@prisma/client";
+
+import { anotherConstellationId } from "./01-constellation.test.js";
+
+let galaxyId;
+export let anotherGalaxyId; // Exported for use in other test files
+
+console.log(anotherConstellationId)
+describe("Galaxies", () => {
+  it("should reject non-string name", async () => {
+    const res = await chai
+      .request(app)
+      .post("/api/v1/galaxies")
+      .send({
+        name: 250,
+        type: "SPIRAL",
+        distance: 800,
+        size: 450,
+        brightness: 20,
+        constellationId: anotherConstellationId
+      });
+
+    chai.expect(res.body.message).to.be.equal("Name should be a string");
+  });
+
+  it("should create a valid galaxy", async () => {
+    const res = await chai.request(app).post("/api/v1/galaxies").send({
+        name: "Andromeda",
+        type: "SPIRAL",
+        distance: 800,
+        size: 450,
+        brightness: 20,
+        constellationId: anotherConstellationId
+    });
+
+    chai
+      .expect(res.body.message)
+      .to.be.equal("Galaxy successfully created");
+    galaxyId = res.body.data[0].id;
+  });
+
+  it("should create another valid galaxy", async () => {
+    const res = await chai.request(app).post("/api/v1/galaxies").send({
+        name: "Pinwheel",
+        type: "IRREGULAR",
+        distance: 1e8,
+        size: 20000,
+        brightness: 5,
+        constellationId: anotherConstellationId
+    });
+
+    chai
+      .expect(res.body.message)
+      .to.be.equal("Galaxy successfully created");
+    anotherGalaxyId = res.body.data[0].id;
+  });
+
+  it("should retrieve all galaxies", async () => {
+    const res = await chai.request(app).get("/api/v1/galaxies");
+
+    chai.expect(res.body.data).to.be.an("array");
+  });
+
+  it("should retrieve a galaxy by ID", async () => {
+    const res = await chai
+      .request(app)
+      .get(`/api/v1/galaxies/${galaxyId}`);
+
+    chai.expect(res.body.data.name).to.be.equal("Andromeda");
+  });
+
+  it("should filter galaxies by name", async () => {
+    const res = await chai.request(app).get("/api/v1/galaxies?name=Andromeda");
+
+    chai.expect(res.body.data[0].name).to.be.equal("Andromeda");
+  });
+
+  it("should sort constellations by name", async () => {
+    const res = await chai.request(app).get("/api/v1/galaxies?sortBy=name");
+
+    chai.expect(res.body.data[0].name).to.be.equal("Andromeda");
+  });
+
+  it("should reject non-numeric distance during update", async () => {
+    const res = await chai
+      .request(app)
+      .put(`/api/v1/galaxies/${galaxyId}`)
+      .send({
+        name: "Andromeda",
+        type: "SPIRAL",
+        distance: "Distance",
+        size: 450,
+        brightness: 20,
+        constellationId: anotherConstellationId
+      });
+
+    chai.expect(res.body.message).to.be.equal("Distance should be a number");
+  });
+
+  it("should reject invalid type during update", async () => {
+    const res = await chai
+      .request(app)
+      .put(`/api/v1/galaxies/${galaxyId}`)
+      .send({
+        name: "Andromeda",
+        type: "INVALID",
+        distance: 800,
+        size: 450,
+        brightness: 20,
+        constellationId: anotherConstellationId
+      });
+
+    chai.expect(res.body.message).to.be.equal(`Type must be one of the following: ${Object.values(GalaxyType)}`);
+  });
+
+  it("should update a valid constellation", async () => {
+    const res = await chai
+      .request(app)
+      .put(`/api/v1/galaxies/${galaxyId}`)
+      .send({
+        name: "Updated Andromeda",
+        type: "SPIRAL",
+        distance: 800,
+        size: 450,
+        brightness: 20,
+        constellationId: anotherConstellationId
+      });
+
+    chai
+      .expect(res.body.message)
+      .to.be.equal(
+        `Galaxy with the id: ${galaxyId} successfully updated`
+      );
+  });
+
+  it("should delete a galaxy by ID", async () => {
+    const res = await chai
+      .request(app)
+      .delete(`/api/v1/galaxies/${galaxyId}`);
+
+    chai
+      .expect(res.body.message)
+      .to.be.equal(
+        `Galaxy with the id: ${galaxyId} successfully deleted`
+      );
+  });
+});
