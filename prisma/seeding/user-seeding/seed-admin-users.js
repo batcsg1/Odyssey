014775c0c2d2dbbf94449a212a9935541078853a
SamBat2005 <batcsg1@student.op.ac.nz>
2025-04-29 20:47:22 +1200

    feat: added seeding admin users script

---
+++ prisma/seeding/user-seeding/seed-admin-users.js
@@ -0,0 +1,38 @@
+import fs from 'node:fs';
+
+import bcryptjs from "bcryptjs";
+
+import prisma from "../../client.js";
+
+const file = fs.readFileSync('prisma/seeding/seeding-files/admin.json', 'utf8');
+
+const seedAdminUsers = async () => {
+    try {
+        const userData = JSON.parse(file); // Parse the file into JSON
+    
+        const data = await Promise.all(
+            userData.map(async (user) => {
+                const salt = await bcryptjs.genSalt();
+                const hashedPassword = await bcryptjs.hash(user.password, salt);
+        
+                return { 
+                    ...user,
+                    password: hashedPassword  // <-- replace plain password with hashed
+                };
+            })
+        );
+
+        //console.log(data);
+
+        await prisma.user.createMany({
+            data: data,
+            skipDuplicates: true,
+        });
+
+        console.log("Admin Users successfully seeded from CSV file");
+    } catch (err) {
+        console.log("Seeding failed:", err.message);
+    }
+};
+
+seedAdminUsers();
