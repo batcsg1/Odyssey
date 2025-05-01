271db220539ea821e4e569bcaaa07b9fd33372ae
SamBat2005 <batcsg1@student.op.ac.nz>
2025-05-01 19:23:19 +1200

    refactor: added constellation id properly to galaxy test

--- tests/02-galaxy.test.js
+++ tests/02-galaxy.test.js
@@ -3,89 +3,31 @@ import chaiHttp from "chai-http";
 import { describe, it } from "mocha";
 
 import app from "../app.js";
+import prisma from "../prisma/client.js";
 
 const chai = chaiModule.use(chaiHttp);
 
-import { GalaxyType } from "@prisma/client";
-
-let constellationId;
-
 let galaxyId;
-let token;
+let constellationId;
 
 describe("Galaxies", () => {
     before(async () => {
-        it("should reject missing token", async () => {
-            const res = await chai
-                .request(app)
-                .get("/api/v1/constellations");
+        const constellation = await prisma.constellation.findFirst();
 
-            chai.expect(res.body.message).to.be.equal("No token provided");
-        });
-
-        it("should login an admin user, return a token, and not have X-Powered-By header", async () => {
-            const res = await chai
-                .request(app)
-                .post("/api/v1/auth/login")
-                .set("Authorization", `Bearer ${token}`)
-                .send({
-                    emailAddress: "john.doe@example.com",
-                    password: "password123",
-                });
-
-            chai.expect(res).to.have.status(200); // Expect a succesfull response
-
-            chai.expect(res.body.token).to.exist;
-
-            token = res.body.token;
-
-            chai.expect(res).to.not.have.header('x-powered-by') // Expect non-default header
-        });
-
-        it("should create a valid constellation", async () => {
-            const res = await chai
-                .request(app)
-                .post("/api/v1/constellations")
-                .set("Authorization", `Bearer ${token}`)
-                .send({
-                    name: "Gemini",
-                    shape: "Gem",
-                    area: 100,
-                    abbreviation: "Gem"
-                });
-
-            chai
-                .expect(res.body.message)
-                .to.be.equal("Constellation successfully created");
-            constellationId = res.body.data[0].id;
-        });
+        constellationId = constellation.id;
     });
 
-    it("should reject non-string name", async () => {
+    it("should create a valid galaxy", async () => {
         const res = await chai
             .request(app)
             .post("/api/v1/galaxies")
             .send({
-                name: 250,
-                type: "SPIRAL",
-                distance: 800,
-                size: 450,
-                brightness: 20,
-                constellationId
+                name: "Andromeda Galaxy",
+                type: "BARRED_SPIRAL",
+                distance: 2500000.0,
+                size: 125700.0,
+                brightness: 3.44
             });
-
-        chai.expect(res.body.message).to.be.equal("Name should be a string");
-    });
-
-    it("should create a valid galaxy", async () => {
-        const res = await chai.request(app).post("/api/v1/galaxies").send({
-            name: "Andromeda",
-            type: "SPIRAL",
-            distance: 800,
-            size: 450,
-            brightness: 20,
-            constellationId
-        });
 
         chai
             .expect(res.body.message)
@@ -94,22 +36,57 @@ describe("Galaxies", () => {
     });
 
     it("should create another valid galaxy", async () => {
-        const res = await chai.request(app).post("/api/v1/galaxies").send({
-            name: "Pinwheel",
-            type: "IRREGULAR",
-            distance: 1e8,
-            size: 20000,
-            brightness: 5,
-            constellationId
-        });
+        const res = await chai
+            .request(app)
+            .post("/api/v1/galaxies")
+            .send({
+                name: "Milky way",
+                type: "BARRED_SPIRAL",
+                distance: 2500000.0,
+                size: 125700.0,
+                brightness: 3.44,
+                constellationId
+            });
 
         chai
             .expect(res.body.message)
             .to.be.equal("Galaxy successfully created");
     });
 
+    it("should reject non-string name", async () => {
+        const res = await chai
+            .request(app)
+            .post("/api/v1/galaxies")
+            .send({
+                name: 12345,
+                type: "BARRED_SPIRAL",
+                distance: 2500000.0,
+                size: 125700.0,
+                brightness: 3.44
+            });
+
+        chai.expect(res.body.message).to.be.equal("Name should be a string");
+    });
+
+    it("should reject non-numeric distance", async () => {
+        const res = await chai
+            .request(app)
+            .post("/api/v1/galaxies")
+            .send({
+                name: "Andromeda Galaxy",
+                type: "BARRED_SPIRAL",
+                distance: "2500000.0", // invalid type
+                size: 125700.0,
+                brightness: 3.44
+            });
+
+        chai.expect(res.body.message).to.be.equal("Distance should be a number");
+    });
+
     it("should retrieve all galaxies", async () => {
-        const res = await chai.request(app).get("/api/v1/galaxies");
+        const res = await chai
+            .request(app)
+            .get("/api/v1/galaxies");
 
         chai.expect(res.body.data).to.be.an("array");
     });
@@ -119,51 +96,38 @@ describe("Galaxies", () => {
             .request(app)
             .get(`/api/v1/galaxies/${galaxyId}`);
 
-        chai.expect(res.body.data.name).to.be.equal("Andromeda");
+        chai.expect(res.body.data.name).to.be.equal("Andromeda Galaxy");
     });
 
     it("should filter galaxies by name", async () => {
-        const res = await chai.request(app).get("/api/v1/galaxies?name=Andromeda");
+        const res = await chai
+            .request(app)
+            .get("/api/v1/galaxies?name=Andromeda Galaxy");
 
-        chai.expect(res.body.data[0].name).to.be.equal("Andromeda");
+        chai.expect(res.body.data[0].name).to.be.equal("Andromeda Galaxy");
     });
 
     it("should sort galaxies by name", async () => {
-        const res = await chai.request(app).get("/api/v1/galaxies?sortBy=name");
+        const res = await chai
+            .request(app)
+            .get("/api/v1/galaxies?sortBy=name");
 
-        chai.expect(res.body.data[0].name).to.be.equal("Andromeda");
+        chai.expect(res.body.data[0].name).to.be.equal("Andromeda Galaxy");
     });
 
-    it("should reject non-numeric distance during update", async () => {
+    it("should reject non-numeric size during update", async () => {
         const res = await chai
             .request(app)
             .put(`/api/v1/galaxies/${galaxyId}`)
             .send({
-                name: "Andromeda",
-                type: "SPIRAL",
-                distance: "Distance",
-                size: 450,
-                brightness: 20,
-                constellationId
+                name: "Andromeda Galaxy",
+                type: "BARRED_SPIRAL",
+                distance: 2500000.0,
+                size: "125700.0", // invalid type
+                brightness: 3.44
             });
 
-        chai.expect(res.body.message).to.be.equal("Distance should be a number");
-    });
-
-    it("should reject invalid type during update", async () => {
-        const res = await chai
-            .request(app)
-            .put(`/api/v1/galaxies/${galaxyId}`)
-            .send({
-                name: "Andromeda",
-                type: "INVALID",
-                distance: 800,
-                size: 450,
-                brightness: 20,
-                constellationId
-            });
-
-        chai.expect(res.body.message).to.be.equal(`Type must be one of the following: ${Object.values(GalaxyType)}`);
+        chai.expect(res.body.message).to.be.equal("Size should be a number");
     });
 
     it("should update a valid galaxy", async () => {
@@ -171,12 +135,11 @@ describe("Galaxies", () => {
             .request(app)
             .put(`/api/v1/galaxies/${galaxyId}`)
             .send({
-                name: "Updated Andromeda",
-                type: "SPIRAL",
-                distance: 800,
-                size: 450,
-                brightness: 20,
-                constellationId
+                name: "Updated Andromeda Galaxy",
+                type: "BARRED_SPIRAL",
+                distance: 2700000.0,
+                size: 125700.0,
+                brightness: 3.44
             });
 
         chai
