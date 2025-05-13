eececb78f39e96b5b64de2e0eeec089c4326678c
SamBat2005 <batcsg1@student.op.ac.nz>
2025-05-13 20:13:07 +1200

    refactor: added filtering and sorting to satellite swagger docs

--- swagger/satellite.js
+++ swagger/satellite.js
@@ -111,6 +111,129 @@
  *     summary: Get all satellites
  *     tags:
  *       - Satellite
+ *     parameters:
+ *       - in: query
+ *         name: name
+ *         schema:
+ *           type: string
+ *         description: Filter satellites by name
+ *       - in: query
+ *         name: age
+ *         schema:
+ *           type: number
+ *           format: float
+ *         description: Filter satellites by age
+ *       - in: query
+ *         name: mass
+ *         schema:
+ *           type: number
+ *           format: float
+ *         description: Filter satellites by mass
+ *       - in: query
+ *         name: diameter
+ *         schema:
+ *           type: number
+ *           format: float
+ *         description: Filter satellites by diameter
+ *       - in: query
+ *         name: density
+ *         schema:
+ *           type: number
+ *           format: float
+ *         description: Filter satellites by density
+ *       - in: query
+ *         name: type
+ *         schema:
+ *           type: string
+ *         description: Filter satellites by type
+ *       - in: query
+ *         name: atmosphere
+ *         schema:
+ *           type: boolean
+ *         description: Filter satellites by atmosphere
+ *       - in: query
+ *         name: year
+ *         schema:
+ *           type: number
+ *           format: float
+ *         description: Filter satellites by orbital period
+ *       - in: query
+ *         name: perigee
+ *         schema:
+ *           type: number
+ *           format: float
+ *         description: Filter satellites by perigee
+ *       - in: query
+ *         name: apogee
+ *         schema:
+ *           type: number
+ *           format: float
+ *         description: Filter satellites by apogee
+ *       - in: query
+ *         name: tilt
+ *         schema:
+ *           type: number
+ *           format: float
+ *         description: Filter satellites by tilt
+ *       - in: query
+ *         name: minTemp
+ *         schema:
+ *           type: number
+ *           format: float
+ *         description: Filter satellites by minimum temperature
+ *       - in: query
+ *         name: maxTemp
+ *         schema:
+ *           type: number
+ *           format: float
+ *         description: Filter satellites by maximum temperature
+ *       - in: query
+ *         name: gravity
+ *         schema:
+ *           type: number
+ *           format: float
+ *         description: Filter satellites by gravity
+ *       - in: query
+ *         name: day
+ *         schema:
+ *           type: number
+ *           format: float
+ *         description: Filter satellites by rotational period
+ *       - in: query
+ *         name: brightness
+ *         schema:
+ *           type: number
+ *           format: float
+ *         description: Filter satellites by brightness
+ *       - in: query
+ *         name: location
+ *         schema:
+ *           type: number
+ *           format: float
+ *         description: Filter satellites by location
+ *       - in: query
+ *         name: habitable
+ *         schema:
+ *           type: boolean
+ *         description: Filter satellites by habitibility
+ *       - in: query
+ *         name: planetId
+ *         schema:
+ *           type: string
+ *           format: uuid
+ *         description: Filter satellites by planetId
+ *       - in: query
+ *         name: sortBy
+ *         schema:
+ *           type: string
+ *           enum: [id, name, age, mass, diameter, density, type, atmosphere, year, perigee, apogee, tilt, hasSatellites, minTemp, maxTemp, gravity, day, brightness, location, habitable, planetId ]
+ *         description: Field to sort the satellites by (default is 'id')
+ *       - in: query
+ *         name: sortOrder
+ *         schema:
+ *           type: string
+ *           enum: [asc, desc]
+ *         description: Order to sort the satellites by (default is 'asc')
  *     responses:
  *       '200':
  *         description: Success
