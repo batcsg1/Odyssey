d23fdc53b8ef6ec23f2e2214ce7773d6bd16047c
SamBat2005 <batcsg1@student.op.ac.nz>
2025-05-25 20:08:45 +1200

    docs: added user swagger docs

---
+++ swagger/user.js
@@ -0,0 +1,209 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *           format: uuid
+ *           example: "123e4567-e89b-12d3-a456-426614174000"
+ *         firstName:
+ *           type: string
+ *           example: "John"
+ *         lastName:
+ *           type: string
+ *           example: "Doe"
+ *         emailAddress:
+ *           type: string
+ *           format: email
+ *           example: "john.doe@example.com"
+ *         password:
+ *           type: string
+ *           example: "password123"        
+ *         loginAttempts:
+ *           type: integer
+ *           example: 3
+ *         lastLoginAttempt:
+ *           type: string
+ *           format: date-time
+ *           example: "2024-07-14T12:34:56Z"
+ *         role:
+ *           type: string
+ *           example: "NORMAL"
+ *         homePlanet:
+ *           type: string
+ *           format: uuid
+ *           example: "123e4567-e89b-12d3-a456-426614174000"
+ *         createdAt:
+ *           type: string
+ *           format: date-time
+ *           example: "2024-07-14T12:34:56Z"
+ *         updatedAt:
+ *           type: string
+ *           format: date-time
+ *           example: "2024-07-14T12:34:56Z"
+ *   securitySchemes:
+ *     BearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ */
+
+/**
+ * @swagger
+ * /api/v1/users:
+ *   post:
+ *     summary: Create a new user
+ *     tags:
+ *       - User
+ *     security:
+ *       - BearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       '201':
+ *         description: User successfully created
+ *       '400':
+ *         description: User with the same name already exists
+ *       '500':
+ *         description: Internal server error
+ */
+
+/**
+ * @swagger
+ * /api/v1/users:
+ *   get:
+ *     summary: Get all users
+ *     tags:
+ *       - User
+ *     parameters:
+ *       - in: query
+ *         name: firstName
+ *         schema:
+ *           type: string
+ *         description: Filter users by first name
+ *       - in: query
+ *         name: lastName
+ *         schema:
+ *           type: string
+ *         description: Filter users by last name
+ *       - in: query
+ *         name: emailAddress
+ *         schema:
+ *           type: string
+ *         description: Filter users by email address
+ *       - in: query
+ *         name: homePlanet
+ *         schema:
+ *           type: string
+ *           format: uuid
+ *         description: Filter users by home planet
+ *       - in: query
+ *         name: sortBy
+ *         schema:
+ *           type: string
+ *           enum: [id, firstName, lastName, emailAddress, homePlanet ]
+ *         description: Field to sort the users by (default is 'id')
+ *       - in: query
+ *         name: sortOrder
+ *         schema:
+ *           type: string
+ *           enum: [asc, desc]
+ *         description: Order to sort the users by (default is 'asc')
+ *     security:
+ *       - BearerAuth: []
+ *     responses:
+ *       '200':
+ *         description: Success
+ *       '404':
+ *         description: No users found
+ *       '500':
+ *         description: Internal server error
+ */
+
+/**
+ * @swagger
+ * /api/v1/users/{id}:
+ *   get:
+ *     summary: Get a user by id
+ *     tags:
+ *       - User
+ *     security:
+ *       - BearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *           format: uuid
+ *     responses:
+ *       '200':
+ *         description: Success
+ *       '404':
+ *         description: No user found with the provided id
+ *       '500':
+ *         description: Internal server error
+ */
+
+/**
+ * @swagger
+ * /api/v1/users/{id}:
+ *   put:
+ *     summary: Update a user by id
+ *     tags:
+ *       - User
+ *     security:
+ *       - BearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *           format: uuid
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       '200':
+ *         description: User successfully updated
+ *       '404':
+ *         description: No user found with the provided id
+ *       '500':
+ *         description: Internal server error
+ */
+
+/**
+ * @swagger
+ * /api/v1/users/{id}:
+ *   delete:
+ *     summary: Delete a user by id
+ *     tags:
+ *       - User
+ *     security:
+ *       - BearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *           format: uuid
+ *     responses:
+ *       '200':
+ *         description: User successfully deleted
+ *       '404':
+ *         description: No user found with the provided id
+ *       '500':
+ *         description: Internal server error
+ */
