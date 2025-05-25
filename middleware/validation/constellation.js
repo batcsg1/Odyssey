bb95a7308c45cc01e6b05b3df2f18d39fbbda9d5
Samuel Batchelor <batcsg1@student.op.ac.nz>
2025-05-26 11:43:40 +1200

    refactor: updated constellation validation to have post and put validation

--- middleware/validation/constellation.js
+++ middleware/validation/constellation.js
@@ -1,50 +1,83 @@
 import Joi from "joi";
 
-const constellationSchema = Joi.object({
-  name: Joi.string().min(3).max(100).required().messages({
-    "string.base": "Name should be a string",
-    "string.empty": "Name cannot be empty",
-    "string.min": "Name should have a minimum length of {#limit}",
-    "string.max": "Name should have a maximum length of {#limit}",
-    "any.required": "Name is required"
-  }),
-  shape: Joi.string().min(3).max(100).optional().messages({
-    "string.base": "Shape should be a string",
-    "string.empty": "Shape cannot be empty",
-    "string.min": "Shape should have a minimum length of {#limit}",
-    "string.max": "Shape should have a maximum length of {#limit}"
-  }),
-  area: Joi.number().min(0).max(41253.0).optional().messages({
-    "number.base": "Area should be a number",
-    "number.min": "Area should be greater than or equal to {#limit}",
-    "number.max": "Area should be lesser than or equal to {#limit}"
-  }),
-  abbreviation: Joi.string().min(3).max(3).required().messages({
-    "string.base": "Abbreviation should be a string",
-    "string.empty": "Abbreviation cannot be empty",
-    "string.min": "Abbreviation should have a minimum length of {#limit}",
-    "string.max": "Abbreviation should have a maximum length of {#limit}",
-    "any.required": "Abbreviation is required"
-  }),
-});
+const validatePostConstellation = (req, res, next) => {
+  const postSchema = Joi.object({
+    name: Joi.string().min(3).max(100).required().messages({
+      "string.base": "Name should be a string",
+      "string.empty": "Name cannot be empty",
+      "string.min": "Name should have a minimum length of {#limit}",
+      "string.max": "Name should have a maximum length of {#limit}",
+      "any.required": "Name is required",
+    }),
+    shape: Joi.string().min(3).max(100).optional().messages({
+      "string.base": "Shape should be a string",
+      "string.empty": "Shape cannot be empty",
+      "string.min": "Shape should have a minimum length of {#limit}",
+      "string.max": "Shape should have a maximum length of {#limit}",
+    }),
+    area: Joi.number().strict().min(0).max(41253.0).optional().messages({
+      "number.base": "Area should be a number",
+      "number.min": "Area should be greater than or equal to {#limit}",
+      "number.max": "Area should be lesser than or equal to {#limit}",
+    }),
+    abbreviation: Joi.string().min(3).max(3).required().messages({
+      "string.base": "Abbreviation should be a string",
+      "string.empty": "Abbreviation cannot be empty",
+      "string.min": "Abbreviation should have a minimum length of {#limit}",
+      "string.max": "Abbreviation should have a maximum length of {#limit}",
+      "any.required": "Abbreviation is required",
+    }),
+  });
 
-const validateSchema = (schema, isRequired = false) => {
-  return (req, res, next) => {
-    const { error } = isRequired
-      ? schema.required().validate(req.body, { convert: false })
-      : schema.validate(req.body, { convert: false });
+  const { error } = postSchema.validate(req.body);
 
-    if (error) {
-      return res.status(409).json({
-        message: error.details[0].message,
-      });
-    }
+  if (error) {
+    return res.status(409).json({
+      message: error.details[0].message,
+    });
+  }
 
-    next();
-  };
+  next();
 };
 
-const validatePostConstellation = validateSchema(constellationSchema, true);
-const validatePutConstellation = validateSchema(constellationSchema);
+const validatePutConstellation = (req, res, next) => {
+  const putSchema = Joi.object({
+    name: Joi.string().min(3).max(100).optional().messages({
+      "string.base": "Name should be a string",
+      "string.empty": "Name cannot be empty",
+      "string.min": "Name should have a minimum length of {#limit}",
+      "string.max": "Name should have a maximum length of {#limit}",
+      "any.required": "Name is required",
+    }),
+    shape: Joi.string().min(3).max(100).optional().messages({
+      "string.base": "Shape should be a string",
+      "string.empty": "Shape cannot be empty",
+      "string.min": "Shape should have a minimum length of {#limit}",
+      "string.max": "Shape should have a maximum length of {#limit}",
+    }),
+    area: Joi.number().strict().min(0).max(41253.0).optional().messages({
+      "number.base": "Area should be a number",
+      "number.min": "Area should be greater than or equal to {#limit}",
+      "number.max": "Area should be lesser than or equal to {#limit}",
+    }),
+    abbreviation: Joi.string().min(3).max(3).optional().messages({
+      "string.base": "Abbreviation should be a string",
+      "string.empty": "Abbreviation cannot be empty",
+      "string.min": "Abbreviation should have a minimum length of {#limit}",
+      "string.max": "Abbreviation should have a maximum length of {#limit}",
+      "any.required": "Abbreviation is required",
+    }),
+  }).min(1);
+
+  const { error } = putSchema.validate(req.body);
+
+  if (error) {
+    return res.status(409).json({
+      message: error.details[0].message,
+    });
+  }
+
+  next();
+};
 
 export { validatePostConstellation, validatePutConstellation };
