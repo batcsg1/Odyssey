183097a2febcbc24e548ebdc0be9b24d65aa8d85
SamBat2005 <batcsg1@student.op.ac.nz>
2025-05-26 07:52:17 +1200

    refactor: updated user validation to update a min of 1 field

--- middleware/validation/user.js
+++ middleware/validation/user.js
@@ -1,47 +1,79 @@
+import { Role } from "@prisma/client";
 import Joi from "joi";
 
-const userSchema = Joi.object({
-    firstName: Joi.string().required().messages({
-        "string.base": "First name should be a string",
-        "string.empty": "First name cannot be empty",
-        "any.required": "First name is required"
-    }),
-    lastName: Joi.string().required().messages({
-        "string.base": "Last name should be a string",
-        "string.empty": "Last name cannot be empty",
-        "any.required": "Last name is required"
-    }),
-    emailAddress: Joi.string()
-        .pattern(/^[A-Za-z0-9._%+-]+@[a-z]+\.com$/)
-        .required()
-        .messages({
-            "string.pattern.base": "Email must be a valid email address",
-            "any.required": "Email is required"
+const validatePostUser = (req, res, next) => {
+    const postSchema = Joi.object({
+        firstName: Joi.string().required().messages({
+            "string.base": "First name should be a string",
+            "string.empty": "First name cannot be empty",
+            "any.required": "First name is required"
         }),
-    homePlanet: Joi.string().uuid().optional().messages({
-        "string.base": "Home planet ID should be a string",
-        "string.empty": "Home planet ID cannot be empty",
-        "string.guid": "Home planet ID should be a valid UUID"
-    })
-});
+        lastName: Joi.string().required().messages({
+            "string.base": "Last name should be a string",
+            "string.empty": "Last name cannot be empty",
+            "any.required": "Last name is required"
+        }),
+        emailAddress: Joi.string()
+            .pattern(/^[A-Za-z0-9._%+-]+@[a-z]+\.com$/)
+            .required()
+            .messages({
+                "string.pattern.base": "Email must be a valid email address",
+                "any.required": "Email is required"
+            }),
+        password: Joi.string().required().messages({
+            "string.base": "Password should be a string",
+            "string.empty": "Password cannot be empty",
+            "any.required": "Password is required"
+        }),
+        role: Joi.string().valid(...Object.values(Role)).required().messages({
+            "string.base": "Role should be a string",
+            "string.empty": "Role cannot be empty",
+            "any.only": `Role must be one of the following: ${Object.values(Role)}`,
+            "any.required": "Role is required"
+        }),
+        homePlanet: Joi.string().uuid().optional().messages({
+            "string.base": "Home planet ID should be a string",
+            "string.empty": "Home planet ID cannot be empty",
+            "string.guid": "Home planet ID should be a valid UUID"
+        })
+    });
 
-const validateSchema = (schema, isRequired = false) => {
-    return (req, res, next) => {
-        const { error } = isRequired
-            ? schema.required().validate(req.body, { convert: false })
-            : schema.validate(req.body, { convert: false });
+    const { error } = postSchema.validate(req.body);
 
-        if (error) {
-            return res.status(409).json({
-                message: error.details[0].message,
-            });
-        }
+    if (error) {
+        return res.status(409).json({
+            message: error.details[0].message,
+        });
+    }
 
-        next();
-    };
-};
+    next();
+}
 
-const validatePostUser = validateSchema(userSchema, true);
-const validatePutUser = validateSchema(userSchema);
+const validatePutUser = (req, res, next) => {
+    const putSchema = Joi.object({
+        firstName: Joi.string().optional(),
+        lastName: Joi.string().optional(),
+        emailAddress: Joi.string()
+            .pattern(/^[A-Za-z0-9._%+-]+@[a-z]+\.com$/)
+            .optional()
+            .messages({
+                "string.pattern.base": "Email must be a valid email address",
+            }),
+        role: Joi.string().valid(...Object.values(Role)).optional().messages({
+            "any.only": `Role must be one of the following: ${Object.values(Role)}`,
+        }),
+        homePlanet: Joi.string().uuid().optional()
+    }).min(1);
+
+    const { error } = putSchema.validate(req.body);
+
+    if (error) {
+        return res.status(409).json({
+            message: error.details[0].message,
+        });
+    }
+
+    next();
+}
 
 export { validatePostUser, validatePutUser };
