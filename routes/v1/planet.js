7e7a344e5153cf276c7e774c3fb2531165f98878
SamBat2005 <batcsg1@student.op.ac.nz>
2025-06-02 20:08:09 +1200

    docs: added jsdocs for planet route

--- routes/v1/planet.js
+++ routes/v1/planet.js
@@ -5,6 +5,7 @@
 
 import createRouter from "./base.js";
 
+// Import the CRUD functions from the controller
 import {
   createPlanet,
   getPlanets,
@@ -13,12 +14,24 @@ import {
   deletePlanet,
 } from "../../controllers/v1/planet.js";
 
+// Import the POST and PUT validation middleware
 import {
   validatePostPlanet,
   validatePutPlanet,
 } from "../../middleware/validation/planet.js";
 
+// Import the authorisation middleware
 import authorisation from "../../middleware/auth/authorisation.js"
+
+/**
+ * Controller object for the planet model
+ * @type {Object}
+ * @property {Function} get - Getting all planets
+ * @property {Function} getById - Getting a planet by ID
+ * @property {Function} create - Creating a planet
+ * @property {Function} update - Updating a planet
+ * @property {Function} delete - Deleting a planet
+ */
 
 const planetController = {
   get: getPlanets,
@@ -27,6 +40,11 @@ const planetController = {
   update: updatePlanet,
   delete: deletePlanet,
 };
+
+/**
+ * Creates an Express router for the endpoints of the Planet model
+ * @type {Function}
+ */
 
 const planetRouter = createRouter(
   planetController,
