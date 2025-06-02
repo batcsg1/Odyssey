e8e3f44f99cf18fc8de0e58432a91b6a7ef2e1b5
SamBat2005 <batcsg1@student.op.ac.nz>
2025-06-02 18:01:04 +1200

    feat: applied limiting middleware to user base router

--- routes/v1/user.base.js
+++ routes/v1/user.base.js
@@ -4,15 +4,16 @@
  */
 
 import express from "express";
+import { getLimit, cudLimit } from "../../middleware/limiting/limit.js";
 
 const createUserRouter = (controller, postValidator, putValidator) => {
     const router = express.Router();
 
-    router.get("/", controller.get);
-    router.get("/:id", controller.getById);
-    router.post("/", postValidator, controller.create);
-    router.put("/:id", putValidator, controller.update);
-    router.delete("/:id", controller.delete);
+    router.get("/", getLimit, controller.get);
+    router.get("/:id", getLimit, controller.getById);
+    router.post("/", cudLimit, postValidator, controller.create);
+    router.put("/:id", cudLimit, putValidator, controller.update);
+    router.delete("/:id", cudLimit, controller.delete);
 
     return router;
 };
