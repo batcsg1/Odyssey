cffa5090e13d78bc218a5807ff0f062bc00e6301
SamBat2005 <batcsg1@student.op.ac.nz>
2025-06-02 17:59:43 +1200

    refactor: created rate limit middleware in middleware directory

--- routes/v1/base.js
+++ routes/v1/base.js
@@ -4,26 +4,10 @@
  */
 
 import express from "express";
-import rateLimit from "express-rate-limit";
-const GET_WINDOW = 60 * 2000;
-const CUD_WINDOW = 60 * 1000;
-const MAX_GET = 20;
-const MAX_CUD = 20;
+import { getLimit, cudLimit } from "../../middleware/limiting/limit.js";
 
 const createRouter = (controller, postValidator, authorisation, putValidator) => {
     const router = express.Router();
-
-    const getLimit = rateLimit({
-        windowMs: GET_WINDOW, // 2 minutes
-        max: MAX_GET,
-        message: `You have exceeded the number of requests: ${MAX_GET}. Please try again in ${(GET_WINDOW / 60000)} minutes.`
-    });
-
-    const cudLimit = rateLimit({
-        windowMs: CUD_WINDOW, // 1 minute
-        max: MAX_CUD,
-        message: `You have exceeded the number of requests: ${MAX_CUD}. Please try again in ${(MAX_CUD / 60000)} minutes.`
-    });
 
     router.get("/", getLimit, controller.get);
     router.get("/:id", getLimit, controller.getById);
