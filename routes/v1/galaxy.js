c1e317b903f1014e955941d832eae5ee801ff149
SamBat2005 <batcsg1@student.op.ac.nz>
2025-06-02 20:03:33 +1200

    docs: added jsdocs to galaxy route

--- routes/v1/galaxy.js
+++ routes/v1/galaxy.js
@@ -5,6 +5,7 @@
 
 import createRouter from "./base.js";
 
+// Import the CRUD functions from the controller
 import {
   createGalaxy,
   getGalaxies,
@@ -13,12 +14,24 @@ import {
   deleteGalaxy,
 } from "../../controllers/v1/galaxy.js";
 
+// Import the POST and PUT validation middleware
 import {
   validatePostGalaxy,
   validatePutGalaxy,
 } from "../../middleware/validation/galaxy.js";
 
+// Import the authorisation middleware
 import authorisation from "../../middleware/auth/authorisation.js"
+
+/**
+ * Controller object for the galaxy model
+ * @type {Object}
+ * @property {Function} get - Getting all galaxys
+ * @property {Function} getById - Getting a galaxy by ID
+ * @property {Function} create - Creating a galaxy
+ * @property {Function} update - Updating a galaxy
+ * @property {Function} delete - Deleting a galaxy
+ */
 
 const galaxyController = {
   get: getGalaxies,
@@ -27,6 +40,11 @@ const galaxyController = {
   update: updateGalaxy,
   delete: deleteGalaxy,
 };
+
+/**
+ * Creates an Express router for the endpoints of the Galaxy model
+ * @type {Function}
+ */
 
 const galaxyRouter = createRouter(
   galaxyController,
