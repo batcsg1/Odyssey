c4d0c69b28296855f6279e65dd955731d91f3049
SamBat2005 <batcsg1@student.op.ac.nz>
2025-06-02 17:53:42 +1200

    feat: added request limiting to all CRUD routes (non-user routes)

--- routes/v1/base.js
+++ routes/v1/base.js
@@ -4,15 +4,32 @@
  */
 
 import express from "express";
+import rateLimit from "express-rate-limit";
+const GET_WINDOW = 60 * 2000;
+const CUD_WINDOW = 60 * 1000;
+const MAX_GET = 20;
+const MAX_CUD = 20;
 
 const createRouter = (controller, postValidator, authorisation, putValidator) => {
     const router = express.Router();
 
-    router.get("/", controller.get);
-    router.get("/:id", controller.getById);
-    router.post("/", postValidator, authorisation, controller.create);
-    router.put("/:id", putValidator, authorisation, controller.update);
-    router.delete("/:id", authorisation, controller.delete);
+    const getLimit = rateLimit({
+        windowMs: GET_WINDOW, // 2 minutes
+        max: MAX_GET,
+        message: `You have exceeded the number of requests: ${MAX_GET}. Please try again in ${(GET_WINDOW / 60000)} minutes.`
+    });
+
+    const cudLimit = rateLimit({
+        windowMs: CUD_WINDOW, // 1 minute
+        max: MAX_CUD,
+        message: `You have exceeded the number of requests: ${MAX_CUD}. Please try again in ${(MAX_CUD / 60000)} minutes.`
+    });
+
+    router.get("/", getLimit, controller.get);
+    router.get("/:id", getLimit, controller.getById);
+    router.post("/", cudLimit, postValidator, authorisation, controller.create);
+    router.put("/:id", cudLimit, putValidator, authorisation, controller.update);
+    router.delete("/:id", cudLimit, authorisation, controller.delete);
 
     return router;
 };
