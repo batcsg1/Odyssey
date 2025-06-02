1b50f9c12da782f1f479e465d878b973c8650d67
SamBat2005 <batcsg1@student.op.ac.nz>
2025-06-02 19:43:02 +1200

    docs: updated app.js documentation

--- app.js
+++ app.js
@@ -8,60 +8,48 @@ import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 import helmet from "helmet";
 
-//Importing routes
+// Import routes
 import indexRoutes from "./routes/v1/index.js";
-
 import constellationRoutes from "./routes/v1/constellation.js";
-
 import starRoutes from "./routes/v1/star.js";
-
 import planetRoutes from "./routes/v1/planet.js";
-
 import satelliteRoutes from "./routes/v1/satellite.js";
-
 import asteroidRoutes from "./routes/v1/asteroid.js";
-
 import meteoriteRoutes from "./routes/v1/meteorite.js";
-
 import cometRoutes from "./routes/v1/comet.js";
-
 import meteorShowerRoutes from "./routes/v1/meteor_shower.js";
-
 import galaxyRoutes from "./routes/v1/galaxy.js";
-
 import userRoutes from "./routes/v1/user.js";
 
-//Authentication routes
-
+// Import middleware
 import auth from "./middleware/auth.js";
-
 import authRoutes from "./routes/v1/auth.js";
-
 import logger from "./middleware/logger.js";
-
-//JSON validation
-
 import { isContentTypeApplicationJSON } from "./middleware/utils.js";
 
 // Create an Express application
 const app = express();
 
-// Use the PORT environment variable or 3000
+/**
+ * @constant {number} PORT - The port the server listens on
+ */
 const PORT = process.env.PORT || 3000;
 
 // Use JSON validation
 app.use(isContentTypeApplicationJSON);
 
+// Parse URL-encoded and JSON request bodies
 app.use(express.urlencoded({ extended: false })); 
-
 app.use(express.json()); 
 
+// Add security headers using Helmet
 app.use(
   helmet({
     xPoweredBy: true,
   })
 );
 
+// Swagger documentation config
 const swaggerOptions = {
   definition: {
     openapi: "3.0.0",
@@ -85,51 +73,48 @@ const swaggerOptions = {
 
 const swaggerDocs = swaggerJSDoc(swaggerOptions);
 
-//Base URL variable
+/**
+ * @constant {string} baseURL - The base URL for the REST API
+ */
 const baseURL = "/api/v1";
 
+// Log all incoming requests using the custom logger
 app.use((req, res, next) => {
   logger.info(`${req.method} ${req.originalUrl}`);
   next();
 });
 
-//Use routes
+// Use routes
 app.use(baseURL, indexRoutes);
-
 app.use(`${baseURL}/auth`, authRoutes);
 
 app.use(`${baseURL}/constellations`, auth, constellationRoutes);
-
 app.use(`${baseURL}/galaxies`, auth, galaxyRoutes);
-
 app.use(`${baseURL}/stars`, auth, starRoutes);
-
 app.use(`${baseURL}/planets`, auth, planetRoutes);
-
 app.use(`${baseURL}/satellites`, auth, satelliteRoutes);
-
 app.use(`${baseURL}/asteroids`, auth, asteroidRoutes);
-
 app.use(`${baseURL}/meteorites`, auth, meteoriteRoutes);
-
 app.use(`${baseURL}/comets`, auth, cometRoutes);
-
 app.use(`${baseURL}/meteor_showers`, auth, meteorShowerRoutes);
-
 app.use(`${baseURL}/users`, auth, userRoutes);
 
-//Swagger route
+// Swagger route
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-// Start the server on port 3000
+// Start the server on defined port
 app.listen(PORT, () => {
   console.log(
     `Server is listening on port ${PORT}. Visit http://localhost:${PORT}${baseURL}`,
   );
 });
 
-//Link to Stack Overflow code for this:
-//https://stackoverflow.com/questions/26079611/node-js-typeerror-path-must-be-absolute-or-specify-root-to-res-sendfile-failed
+
+/**
+ * Catch for 404 routes by serving a custom error page
+ * @see https://stackoverflow.com/questions/26079611/node-js-typeerror-path-must-be-absolute-or-specify-root-to-res-sendfile-failed
+*/
+
 app.use((req, res) => {
   res.status(404).sendFile('404.html', { root: './handlers'});
 });
