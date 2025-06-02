14cf7492c902604071bf3a19eb3a3f51c2857cbf
SamBat2005 <batcsg1@student.op.ac.nz>
2025-06-02 20:10:58 +1200

    docs: added jsdocs to user model

--- routes/v1/user.js
+++ routes/v1/user.js
@@ -5,6 +5,7 @@
 
 import createUserRouter from "./user.base.js";
 
+// Import the CRUD functions from the controller
 import {
   createUser,
   getUsers,
@@ -13,10 +14,21 @@ import {
   deleteUser,
 } from "../../controllers/v1/user.js";
 
+// Import the POST and PUT validation middleware
 import {
   validatePostUser,
   validatePutUser,
 } from "../../middleware/validation/user.js";
+
+/**
+ * Controller object for the user model
+ * @type {Object}
+ * @property {Function} get - Getting all users
+ * @property {Function} getById - Getting a user by ID
+ * @property {Function} create - Creating a user
+ * @property {Function} update - Updating a user
+ * @property {Function} delete - Deleting a user
+ */
 
 const userController = {
   get: getUsers,
@@ -25,6 +37,11 @@ const userController = {
   update: updateUser,
   delete: deleteUser,
 };
+
+/**
+ * Creates an Express router for the endpoints of the User model
+ * @type {Function}
+ */
 
 const userRouter = createUserRouter(
   userController,
