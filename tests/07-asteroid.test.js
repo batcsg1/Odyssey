d298080c4968f0eab308533e38a3732a4e592a3f
SamBat2005 <batcsg1@student.op.ac.nz>
2025-06-04 21:37:35 +1200

    feat: added asteroid pagination test

--- tests/07-asteroid.test.js
+++ tests/07-asteroid.test.js
@@ -95,6 +95,15 @@ describe("Asteroids", () => {
             .to.be.equal("Asteroid successfully created");
     });
 
+    it("should paginate for 2 asteroids", async () => {
+        const res = await chai
+            .request(app)
+            .get("/api/v1/asteroids?page=1&amount=2")
+            .set("Authorization", `Bearer ${token}`);
+
+        chai.expect(res.body.count).to.be.equal(2);
+    });
+
     it("should reject missing star ID", async () => {
         const res = await chai
             .request(app)
