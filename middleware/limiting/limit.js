5dbaf78cdbd5550775a4f9e58930f6e8006e665b
Samuel Batchelor <batcsg1@student.op.ac.nz>
2025-06-05 16:08:29 +1200

    refactor: removed assignment of rate limit object to constant variable

--- middleware/limiting/limit.js
+++ middleware/limiting/limit.js
@@ -5,19 +5,18 @@
 
 import rateLimit from "express-rate-limit";
 
-// Anonymous function for creating a rate limiter
-
-const createRateLimiter = (windowMs, max) => rateLimit({
+const createRateLimiter = (windowMs, max) =>
+  rateLimit({
     windowMs,
     max,
     message: (req, res) => {
-        res.status(429).json({
-            message: `You have exceeded the number of requests: ${max}. Please try again in ${(windowMs / 60000)} minutes.`
-        })
-    }
-});
+      res.status(429).json({
+        message: `You have exceeded the number of requests: ${max}. Please try again in ${windowMs / 60000} minutes.`,
+      });
+    },
+  });
 
 // GET and GETbyID rate limiting functions
-export const getLimit = () => createRateLimiter(2 * 60 * 1000, 20); 
+export const getLimit = () => createRateLimiter(2 * 60 * 1000, 20);
 // CREATE, UPDATE and DELETE rate limiting functions
 export const cudLimit = () => createRateLimiter(1 * 60 * 1000, 10);
