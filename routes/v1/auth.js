47c295af6566c6698c23077d3e2ad618e13c2864
SamBat2005 <batcsg1@student.op.ac.nz>
2025-06-07 23:36:39 +1200

    feat: applied auth middleware to logout route

--- routes/v1/auth.js
+++ routes/v1/auth.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 
 import { register, login, logout } from "../../controllers/v1/auth.js";
+import auth from "../../middleware/auth.js";
 
 const router = Router();
 
 
 router.route("/register").post(register);
 router.route("/login").post(login);
-router.route("/logout").post(logout);
+router.route("/logout").post(auth, logout);
 
 export default router;
