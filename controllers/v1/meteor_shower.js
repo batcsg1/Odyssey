8b61498c8744e4afeeea801b747a883a3f3bbb10
SamBat2005 <batcsg1@student.op.ac.nz>
2025-06-08 15:09:00 +1200

    docs: added jsdocs to meteor shower controller

--- controllers/v1/meteor_shower.js
+++ controllers/v1/meteor_shower.js
@@ -26,6 +26,19 @@ const selectObject = {
   updatedAt: true
 };
 
+/**
+ * @description This function creates a new meteor shower
+ * @param {object} req - The request object
+ * @param {object} res - The response object
+ * @returns {object} - The response object
+ */
+
+/**
+ * @description This function creates a new meteor shower
+ * @param {object} req - The request object
+ * @param {object} res - The response object
+ * @returns {object} - The response object
+ */
 const createMeteorShower = async (req, res) => {
   try {
     const {
@@ -44,16 +57,16 @@ const createMeteorShower = async (req, res) => {
     }
 
     // Check if constellation exists
-    const constellation = await new Repository("Constellation").findById(constellationId);
-    if (!constellation) {
-      return res.status(404).json({ message: `The constellation with id ${constellationId} was not found` });
-    }
-
-    // Find if a meteor shower is already part of a particular constellation
-    const existingMeteorShower = await new advancedRepository("MeteorShower").findByConstellationId(constellationId);
-
-    if (existingMeteorShower) {
-      return res.status(409).json({ message: `There is already a meteor shower that belongs to constellation with ${constellationId}` });
+    if (constellationId) {
+      const constellation = await new Repository("Constellation").findById(constellationId);
+      if (!constellation) {
+        return res.status(404).json({ message: `The constellation with id ${constellationId} was not found` });
+      }
+      // Find if a meteor shower is already part of a particular constellation
+      const existingMeteorShower = await new advancedRepository("MeteorShower").findByConstellationId(constellationId);
+      if (existingMeteorShower) {
+        return res.status(409).json({ message: `There is already a meteor shower that belongs to constellation with ${constellationId}` });
+      }
     }
 
     // Validate each cometId (if comets are provided)
@@ -76,6 +89,7 @@ const createMeteorShower = async (req, res) => {
       }
     }
 
+    // Create new meteor shower
     const newMeteorShower = await meteorShowerRepository.create({
       ...req.body,
       initialDate: isDateValid(initialDate),
@@ -85,6 +99,7 @@ const createMeteorShower = async (req, res) => {
       asteroids: asteroids.length > 0 ? { connect: asteroids.map(id => ({ id })) } : undefined,
     });
 
+    // Return the newly created meteor shower as the response object
     const meteorShower = await meteorShowerRepository.findById(newMeteorShower.id, selectObject);
 
     return res.status(201).json({
@@ -96,8 +111,15 @@ const createMeteorShower = async (req, res) => {
   }
 };
 
+/**
+ * @description This function gets all meteor showers
+ * @param {object} req - The request object
+ * @param {object} res - The response object
+ * @returns {object} - The response object
+ */
 const getMeteorShowers = async (req, res) => {
   try {
+    // Filtering query parameters
     const filters = {
       name: req.query.name || undefined,
       previousYear: req.query.previousYear || undefined,
@@ -112,11 +134,15 @@ const getMeteorShowers = async (req, res) => {
       constellationId: req.query.constellationId || undefined,
     };
 
+    // Sort query parameters
     const sortBy = req.query.sortBy || "id";
     const sortOrder = req.query.sortOrder === "desc" ? "desc" : "asc";
+
+    // Pagination query parameters
     const page = req.query.page
     const amount = req.query.amount
 
+    // Apply filtering, sorting and pagination to meteor shower model
     const meteorShowers = await meteorShowerRepository.findAll(
       selectObject,
       filters,
@@ -142,9 +168,18 @@ const getMeteorShowers = async (req, res) => {
   }
 };
 
+/**
+ * @description This function gets a meteor shower by ID
+ * @param {object} req - The request object
+ * @param {object} res - The response object
+ * @returns {object} - The response object
+ */
 const getMeteorShower = async (req, res) => {
   try {
+
+    // Find a meteor shower by ID
     const meteorShower = await meteorShowerRepository.findById(req.params.id, selectObject);
+
     if (!meteorShower) {
       return res.status(404).json({ message: `No meteor shower with id: ${req.params.id} found` });
     }
@@ -154,12 +189,80 @@ const getMeteorShower = async (req, res) => {
   }
 };
 
+/**
+ * @description This function updates a meteor shower by ID
+ * @param {object} req - The request object
+ * @param {object} res - The response object
+ * @returns {object} - The response object
+ */
 const updateMeteorShower = async (req, res) => {
   try {
-    const meteorShower = await meteorShowerRepository.update(req.params.id, req.body);
+    const {
+      constellationId,
+      comets = [],  // Using comets from the request body
+      asteroids = [],  // Using asteroids from the request body
+      initialDate,
+      finalDate,
+      peakDate
+    } = req.body;
+
+    function isDateValid(dateStr) {
+      const parsedDateStr = String(dateStr);
+      const date = new Date(parsedDateStr)
+      return !isNaN(date.getTime()) ? date : null;
+    }
+
+    // Check if constellation exists
+    if (constellationId) {
+      const constellation = await new Repository("Constellation").findById(constellationId);
+      if (!constellation) {
+        return res.status(404).json({ message: `The constellation with id ${constellationId} was not found` });
+      }
+      // Find if a meteor shower is already part of a particular constellation
+      const existingMeteorShower = await new advancedRepository("MeteorShower").findByConstellationId(constellationId);
+      if (existingMeteorShower) {
+        return res.status(409).json({ message: `There is already a meteor shower that belongs to constellation with ${constellationId}` });
+      }
+    }
+
+    // Validate each cometId (if comets are provided)
+    if (comets.length > 0) {
+      for (const id of comets) {
+        const comet = await new Repository("Comet").findById(id);
+        if (!comet) {
+          return res.status(404).json({ message: `Comet with id ${id} was not found` });
+        }
+      }
+    }
+
+    // Validate each asteroidId (if asteroids are provided)
+    if (asteroids.length > 0) {
+      for (const id of asteroids) {
+        const asteroid = await new Repository("Asteroid").findById(id);
+        if (!asteroid) {
+          return res.status(404).json({ message: `Asteroid with id ${id} was not found` });
+        }
+      }
+    }
+
+    // Meteor shower to be updated
+    let meteorShower = await meteorShowerRepository.update(
+      req.params.id,
+      {
+        ...req.body,
+        initialDate: isDateValid(initialDate),
+        finalDate: isDateValid(finalDate),
+        peakDate: isDateValid(peakDate),
+        comets: comets.length > 0 ? { connect: comets.map(id => ({ id })) } : undefined,
+        asteroids: asteroids.length > 0 ? { connect: asteroids.map(id => ({ id })) } : undefined,
+      },
+      selectObject
+    );
+
     if (!meteorShower) {
       return res.status(404).json({ message: `No meteor shower with the id: ${req.params.id} found` });
     }
+
     return res.status(200).json({
       message: `Meteor shower with the id: ${req.params.id} successfully updated`,
       data: meteorShower,
@@ -169,12 +272,22 @@ const updateMeteorShower = async (req, res) => {
   }
 };
 
+/**
+ * @description This function deletes a meteor shower by ID
+ * @param {object} req - The request object
+ * @param {object} res - The response object
+ * @returns {object} - The response object
+ */
 const deleteMeteorShower = async (req, res) => {
   try {
+
+    // Meteor shower to delete
     const meteorShower = await meteorShowerRepository.findById(req.params.id);
+
     if (!meteorShower) {
       return res.status(404).json({ message: `No meteor shower with the id: ${req.params.id} found` });
     }
+    
     await meteorShowerRepository.delete(req.params.id);
     return res.status(200).json({ message: `Meteor shower with the id: ${req.params.id} successfully deleted` });
   } catch (err) {
