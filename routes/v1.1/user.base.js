34d66cd67c48d581be83043d720e8a1cb71299bf
SamBat2005 <batcsg1@student.op.ac.nz>
2025-06-22 16:45:00 +1200

    feat: copied v1.0 routes into v1.1 routes

---
+++ routes/v1.1/user.base.js
@@ -0,0 +1,29 @@
+/**
+ * @file This file contains and exports the function necessary for creating the user route
+ * @author Samuel Batchelor
+ */
+
+import express from "express";
+
+/**
+ * Function for creating an Express route
+ * @function createUserRouter
+ * @param {Object} controller - The controller object containing CRUD functions
+ * @param {Function} postValidator - Middleware to validate the POST request body
+ * @param {Function} putValidator - Middleware to validate the PUT request body
+ * @returns {express.Router} Return the configured router to be passed on to app.js
+ */
+
+const createUserRouter = (controller, getLimit, cudLimit, postValidator, putValidator) => {
+    const router = express.Router();
+
+    router.get("/", getLimit(), controller.get);
+    router.get("/:id", getLimit(), controller.getById);
+    router.post("/", cudLimit(), postValidator, controller.create);
+    router.put("/:id", cudLimit(), putValidator, controller.update);
+    router.delete("/:id", cudLimit(), controller.delete);
+
+    return router;
+};
+
+export default createUserRouter;
