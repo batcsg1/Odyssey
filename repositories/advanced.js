8935642ff77e4a1909e1e28427d64b631b13a605
SamBat2005 <batcsg1@student.op.ac.nz>
2025-06-25 10:03:32 +1200

    docs: added jsdocs to advanced functions

--- repositories/advanced.js
+++ repositories/advanced.js
@@ -12,22 +12,39 @@ class advancedRepository extends Repository {
      * Creates an instance of the repository based on the repository class's constructor
      * @param {string} model 
      */
+    
     constructor(model) {
         super(model); // Call the parent class's constructor
     }
 
-    async findOneByForeignKey(model, foreignKeyField, foreignKeyValue) {
+    /**
+     * @description This function finds one model by foreign key
+     * @param {string} model - Database model
+     * @param {string} field - Foreign key field
+     * @param {string} value - Foreign key value
+     * @returns {object} The first matching record with the specified foreign key
+     */
+
+    async findOneByForeignKey(model, field, value) {
         return await prisma[model].findUnique({
             where: { 
-                [foreignKeyField]: foreignKeyValue,
+                [field]: value,
              }
         });
     }
 
-    async findManyByForeignKey(model, foreignKeyField, foreignKeyValue) {
+    /**
+     * @description This function finds many models by foreign key
+     * @param {string} model - Database model
+     * @param {string} field - Foreign key field
+     * @param {string} value - Foreign key value
+     * @returns {<Array>object} An array of matching records with the specified foreign key
+     */
+
+    async findManyByForeignKey(model, field, value) {
         return await prisma[model].findMany({
             where: {
-                [foreignKeyField]: foreignKeyValue,
+                [field]: value,
             },
         });
     }
