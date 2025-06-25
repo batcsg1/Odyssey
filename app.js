bcb0eca7318103565d89bdda5c7254f5f3cddee9
SamBat2005 <batcsg1@student.op.ac.nz>
2025-06-25 21:11:22 +1200

    chore: updated API version in swagger options

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ const swaggerOptions = {
     openapi: "3.0.0",
     info: {
       title: "CelestiDB",
-      version: "1.0.0",
+      version: "1.1",
       description: "An API for accessing data about stars, planets and more.",
       contact: {
         name: "Samuel Batchelor",
