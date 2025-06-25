f22d6dfee03a8fc50d55583a21005a26ccfb203c
SamBat2005 <batcsg1@student.op.ac.nz>
2025-06-25 21:25:28 +1200

    feat: added options middleware

--- middleware/options/general.js
+++ middleware/options/general.js
@@ -1,11 +1,14 @@
 const options = (req, res) => {
     const { role } = req.user;
     const methods = role === "NORMAL" ? "GET,OPTIONS" : "GET,PUT,PATCH,DELETE,OPTIONS";
+    const permissions = role === "NORMAL" ? "GET all, GET by ID" : "GET all, GET by ID, UPDATE by ID, DELETE by ID"
     res.set({
         "Allow": methods,
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": methods,
         "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Access-Control-Role": role,
+        "Access-Control-Permissions": permissions
     });
     res.sendStatus(204);
 };
