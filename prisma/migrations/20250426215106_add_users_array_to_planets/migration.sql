a70e3bc08bc6fddabf50006f59cc233b3c14f116
batcsg1 <batcsg1@student.op.ac.nz>
2025-10-31 21:53:27 +1300

    chore: removed old files that were on repo root

--- prisma/migrations/20250426215106_add_users_array_to_planets/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "User" DROP CONSTRAINT "User_homePlanet_fkey";
-
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "homePlanet" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "User" ADD CONSTRAINT "User_homePlanet_fkey" FOREIGN KEY ("homePlanet") REFERENCES "Planet"("id") ON DELETE SET NULL ON UPDATE CASCADE;
