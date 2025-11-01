4ed7e3a1bd175dff3b62fa59b6c07dce094a9dd4
batcsg1 <batcsg1@student.op.ac.nz>
2025-11-01 21:06:06 +1300

    feat: added intro section

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,7 +1,13 @@
+<script>
+
+</script>
+
 <main>
     <section id="intro">
-        <h1>GO TO INFINITY AND BEYOND</h1>
-        <p>Welcome to your one-stop shop for all the information about our solar system and the wider cosmos.</p>
+        <section>
+            <h1>GO TO INFINITY AND BEYOND</h1>
+            <p>Welcome to your one-stop shop for all the information about our solar system and the wider cosmos.</p>
+        </section>
 
     </section>
 
@@ -17,6 +23,18 @@
     }
     #intro{
         background-color: gainsboro;
-        padding: 1.5em;
+        height: 60em;
+        background-image: url(../lib/GIFs/Voyager_Saturn_encounter_animation.gif);
+        background-repeat: no-repeat;
+        background-size:cover;
+        color: white;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-end;
     }
+    #intro section{
+        padding: 4em 3.5em 4em 3.5em;
+        background-color: rgba(0,0,0,0.5);
+    }
+    
 </style>
