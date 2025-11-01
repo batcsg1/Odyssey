59f2c1849c5727f5bb2e5eecd534533ed77ea25d
batcsg1 <batcsg1@student.op.ac.nz>
2025-11-01 20:34:50 +1300

    feat: added new navbar

---
+++ frontend/src/lib/Nav.svelte
@@ -0,0 +1,31 @@
+<script>
+    import Logo from "$lib/Images/logo-dark.png"
+
+</script>
+
+<header>
+    <img src={Logo} alt="Dark Logo" height="140">
+    <button>☰</button>
+    
+</header>
+
+
+<style>
+    header{
+        background-color: black;
+        padding: 1em;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    button{
+        color: white;
+        padding: 0.5em 0.8em 0.5em 0.8em;
+        background-color: #333;
+        border: none;
+        font-size: xx-large;
+        border-radius: 0.2em;
+        margin-right: 1em;
+        font-weight: bolder;
+    }
+</style>
