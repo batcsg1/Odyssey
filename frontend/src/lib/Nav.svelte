d348fa4712cf84eb906821d4d3f1a0e5a1ff3f98
batcsg1 <batcsg1@student.op.ac.nz>
2025-11-01 20:41:45 +1300

    feat: added transition to logo and hamburger icon

--- frontend/src/lib/Nav.svelte
+++ frontend/src/lib/Nav.svelte
@@ -17,15 +17,32 @@
         display: flex;
         justify-content: space-between;
         align-items: center;
+        
+    }
+    img {
+        transition: ease 0.5s;
+    }
+    img:hover {
+        transition: ease 0.5s;
+        cursor: pointer;
+        transform: scale(1.1);
     }
     button{
         color: white;
         padding: 0.5em 0.8em 0.5em 0.8em;
-        background-color: #333;
+        background-color: #1d1c1cff;
         border: none;
         font-size: xx-large;
         border-radius: 0.2em;
         margin-right: 1em;
         font-weight: bolder;
+        transition: ease 0.5s;
+    }
+    button:hover{
+        background-color: white;
+        color: #333;
+        transition: ease 0.5s;
+        cursor: pointer;
+        transform: scale(1.1);
     }
 </style>
