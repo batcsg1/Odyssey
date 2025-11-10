0092fe7bfee3a8d20eadbefe376c902cb9341e63
batcsg1 <batcsg1@student.op.ac.nz>
2025-11-10 20:20:10 +1300

    feat: added main server side fetch file

---
+++ frontend/src/routes/+layout.server.js
@@ -0,0 +1,8 @@
+import { env } from "$env/dynamic/private";
+const url = env.API_URL || "http://localhost:3000";
+
+export const load = async ({ cookies, fetch }) => {
+  const token = cookies.get("token") || null;
+
+  let user = [];
+}
