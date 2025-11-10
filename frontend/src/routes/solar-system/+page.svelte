91c8766ca1a026bc8cf0e77b0b98c2ca50286611
batcsg1 <batcsg1@student.op.ac.nz>
2025-11-10 20:19:58 +1300

    feat: added stuff to the solar system page

---
+++ frontend/src/routes/solar-system/+page.svelte
@@ -0,0 +1,27 @@
+
+<main>
+    <header>
+        <h1>THE SOLAR SYSTEM</h1>
+    </header>
+    <article id="contents">
+        <section>
+            
+        </section>
+
+
+
+    </article>
+</main>
+
+<style>
+    header {
+        height: 15em;
+        color: white;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-image: url(../../lib/Images/solsys.webp);
+        background-repeat: no-repeat;
+        background-size: cover;   
+    }
+</style>
