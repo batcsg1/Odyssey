a272bd1a226661e40c3bca868c74953ea4574285
batcsg1 <batcsg1@student.op.ac.nz>
2025-12-07 11:11:59 +1300

    refactor: updated footer to include new logo

--- frontend/src/lib/components/Footer.svelte
+++ frontend/src/lib/components/Footer.svelte
@@ -1,9 +1,9 @@
 <script>
-
+    import logo from "$lib/Images/logo-dark.png";
 </script>
 
 <footer>
-    <h2>&#123; odyssey &#125;</h2>
+    <img src={logo} alt="logo" height="100">
 
     <section id="footer-bar">
         Terms | Privacy | Login | Site by <strong>Samuel Batchelor</strong>
@@ -17,25 +17,11 @@
 <style>
     footer {
         background: black;
-        background:
-        repeating-linear-gradient(
-            45deg,
-            rgba(255, 255, 255, 0.05) 0px,
-            rgba(255, 255, 255, 0.05) 10px,
-            rgba(0, 0, 0, 0.1) 10px,
-            rgba(0, 0, 0, 0.1) 20px
-        ),
-        linear-gradient(
-            to bottom,
-            rgba(4, 16, 45, 0.9),   /* deep navy blue at top */
-            rgba(0, 13, 30, 0.8)    /* lighter blue at bottom */
-        );
-        mask-image: linear-gradient(to top, black 85%, transparent 100%);
         display: flex;
         flex-direction: column;
         align-items: center;
         color: white;
-        padding: 1.5em;
+        padding: 3em 1em 3em 1em;
     }
 
     #copyright{
