f9e8854ed49a1740b5360f4c191fd4a84a98d8f2
SamBat2005 <batcsg1@student.op.ac.nz>
2025-12-12 20:28:15 +1300

    styles: added dashed line between footer logo and bar

--- frontend/src/lib/components/Footer.svelte
+++ frontend/src/lib/components/Footer.svelte
@@ -16,13 +16,17 @@
 
 <style>
     footer {
-        background: black;
         display: flex;
         flex-direction: column;
         align-items: center;
         color: white;
-        padding: 3em 1em 3em 1em;
+        padding: 2em 1em 2em 1em;
         background: linear-gradient(to top, rgba(0, 0, 0, 1) 97%, rgba(227, 227, 227, 1) 100%, rgba(233, 233, 233, 1) 96%, rgba(255, 255, 255, 1) 100%);
+    }
+
+    #footer-bar{
+        padding-top: 1em;
+        border-top: 0.1em dashed #666;
     }
 
     #copyright{
