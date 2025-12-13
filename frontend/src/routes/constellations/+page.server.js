32fa37fec303e32756a8f9d1933154c8f3a4a8a5
SamBat2005 <batcsg1@student.op.ac.nz>
2025-12-13 20:28:04 +1300

    feat: added image and styling to constellation page

---
+++ frontend/src/routes/constellations/+page.server.js
@@ -0,0 +1,8 @@
+export async function load() {
+    const about = {
+        header: "Constellations",
+        text: `Welcome to the Constellations section of Odyssey! Here, you can explore a curated collection of celestial constellations, each with its own unique story and significance. Whether you're an astronomy enthusiast or just curious about the night sky, our constellation guides provide fascinating insights into the myths, history, and science behind these star patterns. Dive in to discover the wonders of the cosmos and learn how to identify constellations in the night sky. Happy stargazing!`
+    };
+
+    return { about };
+}
---
+++ frontend/src/routes/constellations/+page.svelte
@@ -0,0 +1,48 @@
+<script>
+  let { data } = $props();
+
+  const { about } = data;
+  
+  import Header from "$lib/components/Header.svelte";
+  import Section from "$lib/components/Section.svelte";
+
+  import { page } from "$app/stores";
+  let currentPath = $derived($page.url.pathname);
+  let location = currentPath.replace("/", "");
+
+  console.log(location);
+</script>
+
+<main>
+  <header>
+    <Header {location} />
+  </header>
+  <article>
+    <Section header={about.header} text={about.text}></Section>
+  </article>
+</main>
+
+<style>
+    header {
+    position: relative;
+    color: white;
+    background-image: url(../../lib/Images/constellation.webp);
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    height: 20em;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+  }
+
+   article {
+    background-color: rgba(0, 0, 0);
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    color: white;
+    padding: 3em;
+    gap: 2.2em;
+  }
+</style>
