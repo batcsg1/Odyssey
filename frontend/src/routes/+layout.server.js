235f8fde65421e0e47cd12fb592851fa90932505
SamBat2005 <batcsg1@student.op.ac.nz>
2025-12-14 18:42:24 +1300

    chore: added frontend .env.example

--- frontend/src/routes/+layout.server.js
+++ frontend/src/routes/+layout.server.js
@@ -1,9 +1,22 @@
 import { env } from "$env/dynamic/private";
-const url = env.API_URL || "http://localhost:3000";
+import { error } from "@sveltejs/kit";
+const url = env.API_BASE_URL || "http://localhost:3000";
 
 export const load = async ({ cookies, fetch }) => {
   const token = cookies.get("token") || null;
 
-  let user = [];
+  try {
+    const res = await fetch(`${url}/constellations`);
+    const constellations = await res.json();
 
+    return {
+      constellations,
+      error: null
+    };
+  } catch (err) {
+    return {
+      constellations: [],
+      error: err.message
+    }
+  }
 }
