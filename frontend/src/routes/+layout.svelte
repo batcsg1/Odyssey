626179e880824ae7eda578876c57c5e2f878ae48
SamBat2005 <batcsg1@student.op.ac.nz>
2025-12-25 21:16:47 +1300

    chore: added auth page handler on main layout.svelte

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,31 +1,42 @@
 <script>
-	import "../app.css";
-	import favicon from "$lib/assets/favicon.svg";
-	import Nav from "$lib/components/Nav.svelte";
-	import Footer from "$lib/components/Footer.svelte";
+  import "../app.css";
+  import favicon from "$lib/assets/favicon.svg";
+  import Nav from "$lib/components/Nav.svelte";
+  import Footer from "$lib/components/Footer.svelte";
+  import { page } from "$app/stores";
 
-	let { children } = $props();
+  const authPages = ["/login", "/register"];
+
+  let currentPath = $derived($page.url.pathname);
+
+  let isPublicPage = $derived(authPages.includes($page.url.pathname));
+
+  let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+  <link rel="icon" href={favicon} />
 </svelte:head>
 
-<Nav />
-{@render children()}
-<Footer />
+{#if isPublicPage}
+  {@render children()}
+{:else}
+  <Nav />
+  {@render children()}
+  <Footer />
+{/if}
 
 <style>
-	:global(body) {
-		font-family: "Inter", sans-serif;
-		font-optical-sizing: auto;
-		font-weight: 400;
-		font-style: normal;
-	}
-	:global(button, a) {
-		font-family: "Inter", sans-serif;
-		font-optical-sizing: auto;
-		font-weight: 300;
-		font-style: normal;
-	}
+  :global(body) {
+    font-family: "Inter", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 400;
+    font-style: normal;
+  }
+  :global(button, a) {
+    font-family: "Inter", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 300;
+    font-style: normal;
+  }
 </style>
