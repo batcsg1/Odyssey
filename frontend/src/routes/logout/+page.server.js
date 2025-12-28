f54bd9de93e27486681dc05a5b2f5c0ae72783bb
SamBat2005 <batcsg1@student.op.ac.nz>
2025-12-28 20:03:49 +1300

    chore: added backend logout route call on frontend logout route

--- frontend/src/routes/logout/+page.server.js
+++ frontend/src/routes/logout/+page.server.js
@@ -4,6 +4,18 @@ const url = env.API_BASE_URL || "http://localhost:3000";
 
 export const actions = {
   default: async ({ cookies }) => {
+
+    const token = cookies.get("token");
+
+    if (token) {
+      await fetch(`${url}/auth/logout`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+    }
+
     // Remove the auth token
     cookies.delete("token", {
       path: "/",
