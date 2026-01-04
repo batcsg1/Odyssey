c39120f9eed0224402c0cddf155f7f681f57b377
SamBat2005 <batcsg1@student.op.ac.nz>
2026-01-04 21:47:38 +1300

    styles: changed frontend font to JetBrains Mono

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -28,13 +28,13 @@
 
 <style>
   :global(body) {
-    font-family: "Inter", sans-serif;
+    font-family: "JetBrains Mono", monospace;
     font-optical-sizing: auto;
     font-weight: 400;
     font-style: normal;
   }
   :global(button, a) {
-    font-family: "Inter", sans-serif;
+    font-family: "JetBrains Mono", monospace;
     font-optical-sizing: auto;
     font-weight: 300;
     font-style: normal;
