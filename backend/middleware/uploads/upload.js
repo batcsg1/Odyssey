c7a89e613683a341fbdc04159e34a44a836a78c7
SamBat2005 <batcsg1@student.op.ac.nz>
2026-01-06 12:36:21 +1300

    chore: moved multer logic into separate upload middleware

---
+++ backend/middleware/uploads/upload.js
@@ -0,0 +1,41 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        const timeStamp = new Date().toISOString().replace(/[^0-9]/g, '').slice(0, -3);
+
+        const ext = file.originalname.split('.').pop();
+
+        cb(null, `${timeStamp}.${ext}`);
+    }
+});
+
+export const upload = multer({
+    storage,
+    limits: { fileSize: 5 * 1024 * 1024 } // 5 MB
+});
+
+export const request = (req, res) => {
+    try {
+        console.log("Upload hit:", req.originalUrl);
+        console.log("File:", req.file);
+
+        let { filename, destination } = req.file
+
+        if (!req.file) {
+            return res.status(400).json({ message: "No file provided" });
+        }
+
+        return res.status(200).json({
+            file: req.file,
+            path: `${req.protocol}://${req.get("host")}/${destination}${filename}`
+        });
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message,
+        });
+    }
+}
--- backend/routes/v1.0.0/upload.js
+++ backend/routes/v1.0.0/upload.js
@@ -1,47 +1,8 @@
-import multer from "multer";
 import express from "express";
+import { upload, request } from "../../middleware/uploads/upload.js"
 
 const router = express.Router();
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/');
-    },
-    filename: (req, file, cb) => {
-        const timeStamp = new Date().toISOString().replace(/[^0-9]/g, '').slice(0, -3);
-
-        const ext = file.originalname.split('.').pop();
-
-        cb(null, `${timeStamp}.${ext}`);
-    }
-});
-
-const upload = multer({
-    storage,
-    limits: { fileSize: 5 * 1024 * 1024 } // 5 MB
-});
-
-router.post("/", upload.single("file"), (req, res) => {
-    try {
-        console.log("Upload hit:", req.originalUrl);
-        console.log("File:", req.file);
-
-        let { filename, destination } = req.file
-
-        if (!req.file) {
-            return res.status(400).json({ message: "No file provided" });
-        }
-
-        return res.status(200).json({
-            file: req.file,
-            path: `${req.protocol}://${req.get("host")}/${destination}${filename}`
-        });
-    } catch (err) {
-        return res.status(500).json({
-            message: err.message,
-        });
-    }
-});
-
+router.post("/", upload.single("file"), request );
 
 export default router;
