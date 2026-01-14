86c86c4e7c2dead26d914ca8f3bd48319e9db544
batcsg1 <batcsg1@student.op.ac.nz>
2026-01-14 18:15:40 +1300

    refactor: added back JetBrains Mono as main website font

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -28,13 +28,13 @@
 
 <style>
   :global(body) {
-    font-family: "Inter", monospace;
+    font-family: "JetBrains Mono", monospace;
     font-optical-sizing: auto;
     font-weight: 400;
     font-style: normal;
   }
   :global(button, a) {
-    font-family: "Inter", monospace;
+    font-family: "JetBrains Mono", monospace;
     font-optical-sizing: auto;
     font-weight: 300;
     font-style: normal;
