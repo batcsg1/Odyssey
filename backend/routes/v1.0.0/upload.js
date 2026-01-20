d4aa33c00eca7cf10b771adc28269531dca58f77
batcsg1 <batcsg1@student.op.ac.nz>
2026-01-20 16:11:30 +1300

    fix: added proper multer error handling for uploads route

--- backend/routes/v1.0.0/upload.js
+++ backend/routes/v1.0.0/upload.js
@@ -1,9 +1,21 @@
 import express from "express";
 import upload from "../../middleware/uploads/upload.js"
 import handleUpload from "../../controllers/v1.0.0/upload.js"
+import multer from "multer";
 
 const router = express.Router();
 
-router.post("/", upload.single("file"), handleUpload);
+router.post("/", (req, res) => {
+    upload.single("file")(req, res, (err) => {
+        if (err instanceof multer.MulterError) {
+            // A Multer error occurred (e.g., file size limit exceeded)
+            return res.status(400).json({ message: err.message, code: err.code });
+        } else if (err) {
+            // An unknown error occurred
+            return res.status(500).json({ message: 'An unknown error occurred: ' + err.message });
+        }
+        return handleUpload(req, res);
+    })
+});
 
 export default router;
