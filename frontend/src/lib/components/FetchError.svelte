c0192a2f16530406f42609ec56247c6a8e31499a
batcsg1 <batcsg1@student.op.ac.nz>
2026-01-20 16:39:13 +1300

    chore: added shared fetch error component

---
+++ frontend/src/lib/components/FetchError.svelte
@@ -0,0 +1,19 @@
+<script>
+    let { error = "" } = $props();
+</script>
+
+<p class="error">{error}</p>
+
+<style>
+    .error {
+        background-color: rgba(255, 6, 6, 0.2);
+        border: 0.1em dashed #ff6666;
+        padding: 1em;
+        border-radius: 0.3em;
+        box-shadow: 6px 6px 2em #ff6666;
+        color: #ff6666;
+        width: 30em;
+        text-align: center;
+        font-weight: bolder;
+    }
+</style>
