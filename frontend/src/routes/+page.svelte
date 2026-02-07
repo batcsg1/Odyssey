23ec1ee8c79f7891b4a1ed3569f210c64281e380
SamBat2005 <batcsg1@student.op.ac.nz>
2026-02-07 23:31:53 +1300

    refactor: added more modest green alien-ish style for main page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,79 +1,47 @@
 <script>
+  import Voyager_Saturn from "$lib/GIFs/Voyager_Saturn_encounter_animation.gif"
 </script>
 
 <main>
   <section class="page-sections" id="intro">
     <section>
-      <h2>GO TO INFINITY AND BEYOND</h2>
+      <img src={Voyager_Saturn} alt="img" height=250>
+      <h2>Go to Infinity and Beyond!</h2>
       <p>
         Welcome to your one-stop shop for all the information about our solar
-        system and the wider cosmos.
-      </p>
-    </section>
-  </section>
-
-  <section class="page-sections" id="sol-sys">
-    <section>
-      <h2>EXPLORE THE UNIVERSE</h2>
-      <p>
-        Discover and/or contribute to the latest information about all the
-        fascinating entities that reside throughout the universe, such as red
-        supergiant stars, spiral shaped galaxies, exoplanets, red dwarfs any
-        many more.
+        system and the wider cosmos. Discover and/or contribute to the latest
+        information about all the fascinating entities that reside throughout
+        the universe, such as red supergiant stars, spiral shaped galaxies,
+        exoplanets, red dwarfs any many more.
       </p>
       <button>
-        <a href="/the-universe">EXPLORE ▶</a>
-      </button>
-    </section>
-  </section>
-
-  <section class="page-sections" id="interstellar">
-    <section>
-      <h2>THE INTERSTELLAR</h2>
-      <p>
-        Venture beyond our Solar System and explore the latest discoveries out
-        in the vast realm of interstellar space.
-      </p>
-      <button>
-        <a href="/interstellar">EXPLORE ▶</a>
+        <a href="/the-universe">Explore ▶</a>
       </button>
     </section>
   </section>
 </main>
 
 <style>
-  #intro h2 {
-    background-color: white;
-    color: #333;
+  img{
+    margin-bottom: 1em;
+    border: 0.1em dashed #333;
   }
-
   h2 {
-    font-weight: bold;
-    font-size: 2.5em;
-    padding: 0.2em 0.5em 0.2em 0.5em;
+    font-weight: 600;
+    font-size: 2.8em;
+    color: rgb(2, 227, 2);
+    padding: 0em 0.6em 0em 0.6em;
+    border-bottom: 0.05em dashed #333;
   }
   .page-sections {
-    height: 40em;
     color: white;
     display: flex;
     flex-direction: column;
     justify-content: flex-end;
   }
-  #intro {
-    background-image: url(../lib/GIFs/Voyager_Saturn_encounter_animation.gif);
-    background-repeat: no-repeat;
-    background-size: cover;
-  }
 
-  #sol-sys {
-    background-image: url(https://i.pinimg.com/originals/bb/6c/0e/bb6c0ebb515d9367bff85b5aeb7f2f2b.gif);
-    background-repeat: no-repeat;
-    background-size: cover;
-  }
   .page-sections section {
     padding: 4em 3.5em 4em 3.5em;
-    background-color: rgba(0, 0, 0, 0.7);
-    mask-image: linear-gradient(to top, black 95%, transparent 100%);
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -81,34 +49,27 @@
 
   .page-sections p {
     color: white;
-    padding: 1em 1em 1em 1em;
     max-width: 50em;
-  }
-
-  #interstellar {
-    background-image: url(../lib/GIFs/exoplanet.gif);
-    background-repeat: no-repeat;
-    background-size: cover;
+    text-align: center;
+    padding: 0.8em;
   }
 
   button {
-    padding: 1em 3em 1em 3em;
+    padding: 0.4em 1em 0.4em 1em;
     margin-top: 1.5em;
-    font-size: large;
+    font-size: medium;
     background-color: black;
-    color: white;
-    border: 3px solid white;
+    border: 3px solid rgb(2, 227, 2);
     transition: 0.3s ease;
-    box-shadow: 3px 3px 0px white;
   }
 
   a {
-    color: white;
+    color: rgb(2, 227, 2);
   }
 
   button:hover {
     transition: 0.3s ease;
-    background-color: white;
+    background-color: rgb(2, 227, 2);
     transform: scale(1.1);
     box-shadow: 5px 5px 0px black;
   }
@@ -116,4 +77,6 @@
   button:hover a {
     color: #333;
   }
+
+ 
 </style>
