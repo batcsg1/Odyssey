cb19fe3a4122e07bc0257f4f4119278b9aac35f3
SamBat2005 <batcsg1@student.op.ac.nz>
2026-02-11 21:07:20 +1300

    styles:  changed width and padding of fetch error component

--- frontend/src/lib/components/table/FetchError.svelte
+++ frontend/src/lib/components/table/FetchError.svelte
@@ -8,11 +8,11 @@
     .error {
         background-color: rgba(255, 6, 6, 0.2);
         border: 0.1em dashed #ff6666;
-        padding: 1em;
+        padding: 2em;
         border-radius: 0.3em;
         box-shadow: 6px 6px 2em #ff6666;
         color: #ff6666;
-        width: 30em;
+        width: 25em;
         text-align: center;
         font-weight: bolder;
     }
