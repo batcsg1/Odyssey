e6b484473123602455f24e19854842e6430e8ade
SamBat2005 <batcsg1@student.op.ac.nz>
2026-02-17 13:54:38 +1300

    feat(Content): added background url prop for Content component

--- frontend/src/lib/components/Content.svelte
+++ frontend/src/lib/components/Content.svelte
@@ -1,8 +1,8 @@
 <script>
-  let { children } = $props();
+  let { children, bgUrl = "" } = $props();
 </script>
 
-<div class="content-wrapper">
+<div class="content-wrapper" style="--bg-url: url({bgUrl})">
   {@render children?.()}
 </div>
 
@@ -10,15 +10,24 @@
   .content-wrapper {
     display: flex;
     flex-direction: column;
-    align-items: center;
     justify-content: center;
+    background-image: var(--bg-url);
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+  .content-wrapper :global(main) {
+    background-color: rgba(0,0,0,0.6);
+    display: flex;
+    align-items: center;
+    padding: 2em;
   }
   .content-wrapper :global(h2) {
     font-weight: 400;
     font-size: 2.8em;
     color: white;
     padding: 0em 0.6em 0em 0.6em;
-    border-bottom: 0.05em dashed #333;
+    border-bottom: 0.05em dashed white;
     text-align: center;
     background: -webkit-linear-gradient(#ffffff, #c0bcbc);
     -webkit-background-clip: text;
@@ -30,7 +39,7 @@
     font-size: 2em;
     color: white;
     padding: 0em 0.6em 0em 0.6em;
-    border-bottom: 0.05em dashed #333;
+    border-bottom: 0.05em dashed white;
     text-align: center;
     background: -webkit-linear-gradient(#ffffff, #c0bcbc);
     -webkit-background-clip: text;
@@ -50,6 +59,8 @@
     background-color: black;
     border: 3px solid white;
     transition: 0.3s ease;
+    max-width: 10em;
+    align-self: center;
   }
 
   .content-wrapper :global(a) {
@@ -67,7 +78,7 @@
     color: #333;
   }
   .content-wrapper :global(img) {
-    margin-bottom: 1em;
     box-shadow: 2px 2px 2em rgb(30, 15, 53);
+    align-self: center;
   }
 </style>
