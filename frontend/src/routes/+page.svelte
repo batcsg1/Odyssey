7923e08d5f08123d7e4b6aa6c4839671dcda4f0f
SamBat2005 <batcsg1@student.op.ac.nz>
2026-02-17 13:29:56 +1300

    refactor(main): added earth horizon as background image

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,21 +1,35 @@
 <script>
   import Content from "$lib/components/Content.svelte";
   import Voyager_Saturn from "$lib/GIFs/Voyager_Saturn_encounter_animation.gif";
+  import Horizon from "$lib/Images/horizon.jpg"
 </script>
 
-<Content>
-  <img src={Voyager_Saturn} alt="img" height="250" />
+<main style="--bg-url: url({Horizon})">
+  <Content>
+    <img src={Voyager_Saturn} alt="img" height="250" />
 
-  <h2>Go to Infinity and Beyond!</h2>
-  <p>
-    Welcome to your one-stop shop for all the information about our solar
-    system and the wider cosmos. Discover and/or contribute to the latest
-    information about all the fascinating entities that reside throughout the
-    universe, such as red supergiant stars, spiral shaped galaxies,
-    exoplanets, red dwarfs any many more.
-  </p>
-  <button>
-    <a href="/the-universe">Explore ▶</a>
-  </button>
-</Content>
+    <h2>Go to Infinity and Beyond!</h2>
+    <p>
+      Welcome to your one-stop shop for all the information about our solar
+      system and the wider cosmos. Discover and/or contribute to the latest
+      information about all the fascinating entities that reside throughout the
+      universe, such as red supergiant stars, spiral shaped galaxies,
+      exoplanets, red dwarfs any many more.
+    </p>
+    <button>
+      <a href="/the-universe">Explore ▶</a>
+    </button>
+  </Content>
+</main>
+
+<style>
+  main {
+    background-image: var(--bg-url);
+    height: 100vh;
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
 
